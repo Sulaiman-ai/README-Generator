b179a88522a7079f5e8918297716aa6dd4f19c35
Sulaiman Amjad <sulaiman.amjad@gmail.com>
2023-01-23 11:45:18 +0000

    added initial questions and prompt

--- index.js
+++ index.js
@@ -5,6 +5,36 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+    {
+        type: 'input',
+        name: 'project_title',
+        message: 'What is the title of your project',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter your project description',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter installation instructions for your project',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage information',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Enter contribution guidelines',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Enter your test instructions',
+    },
 
 ];
 
@@ -14,7 +44,9 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions).then((answers) => {
+        console.log(JSON.stringify(answers, null, '  '));
+      });
 }
 
 // function call to initialize program
